940c30ebf66372864018c540eb1af21549f74e2f
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-07-14 16:54:12 -0400

    action added, typo removed

--- recipes/guest_sysadmins.rb
+++ recipes/guest_sysadmins.rb
@@ -6,7 +6,8 @@
 include_recipe('users::sysadmins')
 
 users_manage "guest_sysadmin" do
-  data_bag "guest_user"
+  data_bag "guest_users"
   group_name "sysadmin"
+  action [:remove, :create]
   group_id 2300
 end
