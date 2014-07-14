00c9fdf1f89a154dfcc32864da25bef0f2bc37c9
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-07-14 16:35:16 -0400

    version bump 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dmozzherin@gmail.com'
 license          'MIT'
 description      'Manages specific users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'users'
