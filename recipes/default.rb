cfec36e9fc95a124af05eaad7ebe549cb1ef7bde
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-02-11 00:12:41 -0500

    nil stops recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,8 @@
 groups = data_bag_item("eol-users", "groups")
 
-exit unless groups
-
 groups["groups"].each do |group|
   eol_users_manage group["group_name"] do
     group_id group["group_id"] if group["group_id"]
     action group["action"] || [:remove, :create]
   end
-end
+end if groups
